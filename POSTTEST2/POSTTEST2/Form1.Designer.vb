c6d002a0eb035b97313e22a2ee71f7099cdb4ccc
112RifqiAlBukhari <rifqialbukhari32@gmail.com>
2026-03-05 14:20:40 +0800

    posttest 2

---
+++ POSTTEST2/POSTTEST2/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label5 = New Label()
+        txtJudul = New TextBox()
+        txtGenre = New TextBox()
+        txtHapus = New TextBox()
+        btnTambah = New Button()
+        btnHapus = New Button()
+        listBuku = New ListBox()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(23, 48)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(79, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Judul Buku"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(28, 108)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(48, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Genre"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(33, 48)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(79, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Judul Buku"
+        ' 
+        ' txtJudul
+        ' 
+        txtJudul.Location = New Point(131, 44)
+        txtJudul.Name = "txtJudul"
+        txtJudul.Size = New Size(125, 27)
+        txtJudul.TabIndex = 5
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(131, 106)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(125, 27)
+        txtGenre.TabIndex = 6
+        ' 
+        ' txtHapus
+        ' 
+        txtHapus.Location = New Point(136, 48)
+        txtHapus.Name = "txtHapus"
+        txtHapus.Size = New Size(125, 27)
+        txtHapus.TabIndex = 7
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(200, 145)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 7
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnHapus
+        ' 
+        btnHapus.Location = New Point(191, 143)
+        btnHapus.Name = "btnHapus"
+        btnHapus.Size = New Size(94, 29)
+        btnHapus.TabIndex = 8
+        btnHapus.Text = "Hapus"
+        btnHapus.UseVisualStyleBackColor = True
+        ' 
+        ' listBuku
+        ' 
+        listBuku.FormattingEnabled = True
+        listBuku.Location = New Point(231, 232)
+        listBuku.Name = "listBuku"
+        listBuku.Size = New Size(316, 164)
+        listBuku.TabIndex = 11
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.ActiveCaption
+        GroupBox1.Controls.Add(btnTambah)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(txtGenre)
+        GroupBox1.Controls.Add(txtJudul)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Location = New Point(64, 22)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(302, 180)
+        GroupBox1.TabIndex = 12
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Tambah Buku"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = SystemColors.ActiveCaption
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(txtHapus)
+        GroupBox2.Controls.Add(btnHapus)
+        GroupBox2.Location = New Point(452, 22)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(299, 180)
+        GroupBox2.TabIndex = 13
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Hapus Buku"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(listBuku)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJudul As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents txtHapus As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents listBuku As ListBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+
+End Class
---
+++ POSTTEST2/POSTTEST2/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Sub TambahBuku(ByVal buku As String)
+
+        daftarBuku(jumlahBuku) = buku
+        jumlahBuku += 1
+
+    End Sub
+    Function FormatBuku(judul As String, genre As String) As String
+        Return judul & " (" & genre & ")"
+    End Function
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim judul As String = txtJudul.Text
+        Dim genre As String = txtGenre.Text
+        Dim data As String
+        data = FormatBuku(judul, genre)
+        TambahBuku(data)
+        listBuku.Items.Add(data)
+        txtJudul.Clear()
+        txtGenre.Clear()
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        Dim judul As String = txtHapus.Text
+        For i As Integer = 0 To jumlahBuku - 1
+            If daftarBuku(i).Contains(judul) Then
+                listBuku.Items.Remove(daftarBuku(i))
+                For j As Integer = i To jumlahBuku - 2
+                    daftarBuku(j) = daftarBuku(j + 1)
+                Next
+                jumlahBuku -= 1
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
